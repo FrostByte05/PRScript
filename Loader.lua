59d861f518856a7f239edd4dd93763b56dd85687
FrostByte <136031730+FrostByte05@users.noreply.github.com>
2024-06-07 13:48:17 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,16 @@
+local Key = "testkey"
 
+local Keys = loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostByte05/PRScript/main/Keys.lua"))()
+for i, v in Keys.ValidKeys do
+	key = v
+end
+
+local function CheckKey()
+	if string.find(Key, key) then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostByte05/PRScript/main/MainSource.Lua"))()
+	else
+		game.Players.LocalPlayer:Kick("Wrong key -w-")
+	end
+end
+
+CheckKey()
